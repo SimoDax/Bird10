e24ecc8ab24a031086fd1d6b72534a49d8e36980
SimoDax <s.dassi.pub@gmail.com>
2020-10-05 14:18:38 +0200

    Fixed scrolling when loading additional tweets in a conversation
    The signal notifying that the initial loading of a conversation is completed isn't emitted anymore when additional replies are loaded, i.e. when the main tweet id isn't found among all the tweets returned in the response

--- Bird10/src/ConversationApi.cpp
+++ Bird10/src/ConversationApi.cpp
@@ -30,6 +30,7 @@ ConversationApi::ConversationApi(QObject *parent) : TwitterApi(parent)
 void ConversationApi::loadConversation(const QString& id, bool includeCursor)
 {
     m_id = id;
+    m_mainTweetIndex = -1;
     QString request("https://api.twitter.com/2/timeline/conversation/" + id + ".json?include_profile_interstitial_type=1&include_blocking=1&include_blocked_by=1&include_followed_by=1&include_want_retweets=1&include_mute_edge=1&include_can_dm=1&include_can_media_tag=1&skip_status=1&cards_platform=Web-12&include_cards=1&include_composer_source=true&include_ext_alt_text=true&include_reply_count=1&tweet_mode=extended&include_entities=true&include_user_entities=false&include_ext_media_color=false&include_ext_media_availability=false&send_error_codes=true&simple_quoted_tweets=true&count=50&ext=mediaStats,highlightedLabel,cameraMoment");
     QList<O0RequestParameter> par = basicGetParameters();
     par.append(O0RequestParameter("include_user_entities", "false"));
@@ -139,7 +140,8 @@ void ConversationApi::conversationReceived()
     }
 
     emit tweetModelChanged();
-    emit conversationLoaded(m_mainTweetIndex);
+    if(m_mainTweetIndex != -1)
+        emit conversationLoaded(m_mainTweetIndex);  // if the loaded piece of conversation contains the main tweet then notify its position to qml
 
     m_users.clear();
     m_tweets.clear();
